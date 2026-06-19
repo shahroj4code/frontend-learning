15513d282d61e066e0193b3dfe9fff47a1ccd51d
shahroz <moshahjilraza@gmail.com>
2026-06-19 11:07:09 +0530

    restore file

---
+++ level3/shopList.js
@@ -0,0 +1,8 @@
+// console.log(document.getElementById('item-form'));
+// const form = document.getElementById('item-form');
+// form.textContent = 'helo';
+const item = document.getElementById('item-input');
+item.id = 'new id';
+item.type = 'submit';
+item.textContent = '';
+console.log(item);
